e76c5063e5b94f48a6bb96d390437ad682ca30b5
Quinten <quintenvanhoof@gmail.com>
2025-09-15 10:26:12 +0200

    Ticket 7: Style manual links
    Links in the manual are now styled and are more visible. Fitting with the brand links on the home page

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -8,22 +8,25 @@
         <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
     </x-slot:breadcrumb>
 
+    <div class="brand-page">
+        <h1>{{ $brand->name }}</h1>
 
-    <h1>{{ $brand->name }}</h1>
+        <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
 
-    <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
+        <div class="manual-list">
+            @foreach ($manuals as $manual)
+                <li>
+                @if ($manual->locally_available)
+                    
+                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                    ({{$manual->filesize_human_readable}})
+                    
+                @else
+                    <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                @endif
+                </li>
 
-    <div class="manual-list">
-        @foreach ($manuals as $manual)
-
-            @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-                ({{$manual->filesize_human_readable}})
-            @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-            @endif
-
-        @endforeach
+            @endforeach
+        </div>
     </div>
-    
 </x-layouts.app>
