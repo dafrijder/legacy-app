97ba3bb2e09fe63ecf913204b519098efd3b7cbf
Demien47 <D309432@edu.curio.nl>
2025-09-19 14:06:52 +0200

    ticket 12 afgerond - form aangemaakt en stuurt gegevens door naar tekst bestand

--- resources/views/pages/contactformulier.blade.php
+++ resources/views/pages/contactformulier.blade.php
@@ -1,1 +1,32 @@
-<p>hallo</p>
+<x-layouts.app>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contactformulier</title>
+</head>
+<body>
+    <h1>Contactformulier</h1>
+
+    @if(session('success'))
+        <p style="color: green;">{{ session('success') }}</p>
+    @endif
+
+    <form action="/contact" method="POST">
+        @csrf
+
+        <label>Naam:</label><br>
+        <input type="text" name="name" required><br><br>
+
+        <label>E-mail:</label><br>
+        <input type="email" name="email" required><br><br>
+
+        <label>Bericht:</label><br>
+        <textarea name="message" required></textarea><br><br>
+
+        <button type="submit">Verstuur</button>
+    </form>
+</body>
+</html>
+
+</x-layouts.app>
